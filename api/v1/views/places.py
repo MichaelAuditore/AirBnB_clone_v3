bda8e7735c7bb90cf55d740dfcc10d7ab70a06de
MichaelAuditore <michael_v613@hotmail.com>
2020-04-29 02:31:41 -0500

    [Fix] Statements for UPDATE and CREATE Places objects

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,7 +2,7 @@
 """ Places template """
 from flask import request, jsonify, abort
 from api.v1.views import app_views
-from models import storage, city, place
+from models import storage, city, place, user
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
@@ -25,16 +25,19 @@ def get_place(place_id):
         abort(404)
     return (jsonify(g_place.to_dict()), 200)
 
+
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
     """ Delete a city with the given ID """
     d_place = storage.get(place.Place, place_id)
-    if not d_place:
+    if d_place:
+        storage.delete(d_place)
+        storage.save()
+        return (jsonify({}))
+    else:
         abort(404)
-    storage.delete(d_place)
-    storage.save()
-    return (jsonify({}))
+
 
 @app_views.route("/cities/<city_id>/places", methods=['POST'],
                  strict_slashes=False)
@@ -45,9 +48,14 @@ def post_place(city_id):
         abort(400, 'Not a JSON')
     if 'name' not in content:
         abort(400, 'Missing name')
+    if 'user_id' not in content:
+        abort(400, 'Missing user_id')
+
     one_city = storage.get(city.City, city_id)
-    if one_city is None:
+    one_user = storage.get(user.User, content['user_id'])
+    if one_city is None or one_user is None:
         abort(404)
+
     new_place = place.Place(**content)
     new_place.city_id = city_id
     storage.new(new_place)
@@ -59,12 +67,18 @@ def post_place(city_id):
                  strict_slashes=False)
 def put_place(place_id):
     """ Update information in a place if ID exists """
-    content = request.get_json()
-    if content is None:
-        abort(400, 'Not a JSON')
     one_place = storage.get(place.Place, place_id)
     if one_place is None:
         abort(404)
-    setattr(one_place, 'name', content['name'])
+    content = request.get_json()
+    if content is None:
+        abort(400, 'Not a JSON')
+
+    ignore = ['id', 'created_at', 'updated_at', 'city_id', 'user_id']
+
+    for key, val in content.items():
+        for item in ignore:
+            if (key != item):
+                setattr(one_place, key, value)
     storage.save()
     return(jsonify(one_place.to_dict()), 200)
